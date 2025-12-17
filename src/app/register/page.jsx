e9edbb6665d14a490cbc9373d6e1c39f8bfc890a
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-12-17 12:50:52 +0600

    set the dev history project

---
+++ src/app/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h2>welcome to register</h2>
+        </div>
+    );
+};
+
+export default Register;
